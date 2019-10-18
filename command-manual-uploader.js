e441e2480a896eedf0f54cc16c9eb6da3f86cd53
Tigerstar <Tigerball890@gmail.com>
2019-10-18 10:12:57 -0400

    Better command manual formatting

--- command-manual-uploader.js
+++ command-manual-uploader.js
@@ -18,12 +18,12 @@ function get_text(guild_id) {
     module_manuals = module_manuals.pop();
   }
   // Construct text buffer based on that one object
-  var text_buffer = "TigerDyno Command List\r\n======================\r\n\r\nGlobal Prefix: % (use individual server prefix on that server)\r\n\r\n";
+  var text_buffer = "TigerDyno Command List\r\n======================\r\n\r\nGlobal Prefix: % (use individual server prefix on that server, mention bot to learn server prefix)\r\n\r\n";
   var names = Object.keys(manuals_object);
   var prefix = config.global_prefix;
   for (var i = 0; i < names.length; i++) {
     text_buffer += prefix + names[i] + ": ";
-    text_buffer += manuals_object[names[i]] + "\r\n";
+    text_buffer += manuals_object[names[i]] + "\r\n\r\n";
   }
   return text_buffer;
 }
