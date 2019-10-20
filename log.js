69a4076d3d229b5df73f0126de3d864ceba6dca7
Tigerstar <Tigerball890@gmail.com>
2019-10-20 11:20:00 -0400

    Fix logs to use time in front of message, and display year properly

--- log.js
+++ log.js
@@ -1,8 +1,23 @@
 'use strict';
 function log(message) {
-  var fs = require("fs");
-  var date = new Date();
-  var date_string = date.getMonth() + "_" + date.getDate() + "_" +  date.getYear() + ".txt";
+  // Change the date object to a normal clock time, date is already listed in the file name
+  function time(date) {
+   let [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()]
+   if (String(hours).length === 1) {
+     hours = "0" + hours;
+   }
+   if (String(minutes).length === 1) {
+     minutes = "0" + minutes;
+   }
+   if (String(seconds).length === 1) {
+     minutes = "0" + seconds;
+   }
+   return `${hours}:${minutes}:${seconds}`
+  }
+  let fs = require("fs");
+  let date = new Date();
+  message = "[" + time(date) + "] " + message; // Add date in front of message to append to log
+  let date_string = date.getMonth() + "_" + date.getDate() + "_" +  date.getFullYear() + ".txt";
   if (!(fs.existsSync("./logs/" + date_string))) {
     fs.writeFile("./logs/" + date_string, message, 'utf8', function (err) {
       if (err) {
