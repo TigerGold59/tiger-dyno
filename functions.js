3c75ab5707a443323e705673994240073e543226
Tigerstar <Tigerball890@gmail.com>
2019-11-16 15:02:25 -0500

    Finally works, fixed incorrect reference

--- functions.js
+++ functions.js
@@ -211,7 +211,7 @@ async function function_parser(message, client, Discord) {
     }},
     "commands": {"function": function(message, client, Discord) {
       var cmd_manual_txt = require("./command-manual-uploader.js")(message.guild.id);
-      const upload = require("./trickjump_pastebin.js")
+      const upload = require("./upload_to_pastebin.js")
       upload(cmd_manual_txt, function(url) {
         message.channel.send("Command manual here: " + "https://pastebin.com/raw/" + url);
       })
