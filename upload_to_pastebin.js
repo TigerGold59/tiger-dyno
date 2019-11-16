357a1d392cc2d217065180bc174c25b2250c944f
Tigerstar <Tigerball890@gmail.com>
2019-11-16 14:58:52 -0500

    Fix to use token correctly

--- upload_to_pastebin.js
+++ upload_to_pastebin.js
@@ -1,6 +1,6 @@
 async function uploadToPastebin(text, callback) {
   let pastebinAPI = require("pastebin-js")
-  let pastebin = new pastebinAPI(require("./pastebin-dev-token.json"))
+  let pastebin = new pastebinAPI(require("./pastebin-dev-token.json")['token'])
   let data = await pastebin.createPaste(text)
   // Calls callback function with ID, NOT link
   callback(data.split("https://pastebin.com/")[1]);
