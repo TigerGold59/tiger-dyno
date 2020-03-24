3b9f55184ce38d9b2b96b7eeb725641e6aae2936
TigerGold59 <54746609+TigerGold59@users.noreply.github.com>
2020-03-23 22:46:29 -0400

    Better mention detection
    Use discord's message mention API property

--- messages.js
+++ messages.js
@@ -8,7 +8,7 @@ function processMessage(message, client, Discord) {
   functions(message, client, Discord);
 
   // Process other information here like DMs, or mentions.
-  if (message.content.includes("@TigerDyno#3832") || message.content.includes("<@626223136047628308>")) {
+  if (message.mentions.users.has("626223136047628308")) {
     let kv = require("keyv");
     let db = new kv("sqlite://database.sqlite", {"namespace": "prefixes"});
     let getPrefix = async function(id, database) {
